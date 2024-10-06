1b17cd408fc26ff9856eff0ee2f2b5d6527ca12f
Nashra-shakeel <nashrashakeel12@gmail.com>
2024-10-06 01:51:33 -0700

    pd5task1.cpp

--- pd5task1.cpp
+++ pd5task1.cpp
@@ -3,13 +3,9 @@ using namespace std;
 int x=10;
 void myFunction()
 {
-    cout<<"The value of x is:";
     int x=20;
 }
 main()
 {
-    myFunction();
-    int x=30;
     cout<<"The value of x is:"<<x;
-    myFunction();
 }
